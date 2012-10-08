08e8bbb7b81ce0cab405808120ed2774de4e8f42
David <david@rover.chris-cartwright.com>
2012-10-08 13:34:43 +0000

    Added Vehicle State and Video Control Added a temporary class to rover, website, and lib so that the project can be built without errors.

---
+++ src/VehicleLib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VehicleLib
+{
+    class Class1
+    {
+        static int Main(string[] args)
+        {
+            //...
+            return 0;
+        }
+    }
+}
