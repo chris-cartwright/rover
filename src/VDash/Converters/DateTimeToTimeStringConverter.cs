75200bf0c4125346e104f39f237985a8366b505c
Chris Cartwright <cartwright.christopher@gmail.com>
2012-10-22 18:36:35 +0000

    - Forgot to commit Converter

---
+++ src/VDash/Converters/DateTimeToTimeStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace VDash.Converters
+{
+	[ValueConversion(typeof(DateTime), typeof(string))]
+	public class DateTimeToTimeStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value as DateTime? == null)
+				return "";
+
+			DateTime dt = (DateTime)value;
+			return String.Format("[{0}:{1}:{2}] ", dt.Hour, dt.Minute, dt.Second);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
