f4fca6c48341df4218176af27529010e80b0cb74
Rick <rick@rover.chris-cartwright.com>
2012-11-09 04:18:11 +0000

    - Added Login.cs, should have been in last commit by me

---
+++ src/VehicleLib/Login.cs
@@ -0,0 +1,48 @@
+﻿/*
+    Copyright (C) 2012 Christopher Cartwright
+    Copyright (C) 2012 Richard Payne
+    Copyright (C) 2012 Andrew Hill
+    Copyright (C) 2012 David Shirley
+    
+    This file is part of VDash.
+
+    VDash is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    VDash is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with VDash.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VehicleLib
+{
+
+	[Serializable]
+	class Login
+	{
+		private uint _loginId;
+		private string _password;
+
+		public VehiclePipe.OnConnectionHandler Callback;
+
+		public Login() { }
+
+		public Login(uint id, string password)
+		{
+			_loginId = id;
+			_password = password;
+		}
+	}
+}
